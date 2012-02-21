483f1388d58e925b280eaeab7d1b5e1a8d6557e6
Jan Groenewald <jan@aims.ac.za>
2012-02-21 17:42:27 +0100

    Notes inside functions

--- sanitize.py
+++ sanitize.py
@@ -13,14 +13,19 @@ DATADIR="data"
 # FUNCTIONS
 
 def find_data_files(DIR=DATADIR):
+	# exlcude NOTES?
+	# include filenames with at least two consecutive digits?
 	return filelist
 
 def missing_gender(LINE):
+	# return True if contains "Sex: N" else
 	return False
 
 def fix_gender(FILE, LINE)
+	# given FILE and LINE replace LINE with LINE(remove :N replace :M)
 	return file	
 
 # MAIN
 if __name__ == "__main__":
 	print "this is sanitize.py"
+	# loop through and do the necessary
