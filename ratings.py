053e17768af9e267e8027903f017d67deded50d2
Jan Groenewald <jan@aims.ac.za>
2012-02-22 18:57:18 +0100

    Ratings arrray finished

--- ratings.py
+++ ratings.py
@@ -10,7 +10,10 @@ class ratings(object):
   self.get_ratings_array()
 
  def get_ratings_array(self):
-  self.myratings = np.zeros((len(self.mypersons),len(self.mypapers)), dtype=int)
+  self.myratings = np.zeros((len(self.persons),len(self.papers)), dtype = float)
+  for person in self.persons:
+   for paper in self._data[person].keys():
+    self.myratings[self.persons.index(person),self.papers.index(paper)] = self._data[person][paper]
 
  def get_person_ids(self):
   self.persons = self._data.keys()
@@ -19,9 +22,9 @@ class ratings(object):
  def get_paper_ids(self):
   self.papers = []
   for person in self.persons:
-  	for paper in self._data[person].keys():
-  		if paper not in self.papers:
-  			self.papers.append(paper)
+   for paper in self._data[person].keys():
+    if paper not in self.papers:
+     self.papers.append(paper)
   self.papers.sort()
 
  def recommendations():
