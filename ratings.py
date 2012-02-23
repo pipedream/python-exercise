c1379f3c856840e26ac44677616b193ed1b2434d
Jan Groenewald <jan@aims.ac.za>
2012-02-23 13:48:04 +0100

    No syntax errors!

--- ratings.py
+++ ratings.py
@@ -27,13 +27,13 @@ class ratings(object):
    for paper in self._data[person].keys():
     self.myratings[self.persons.index(person),self.papers.index(paper)] = self._data[person][paper]
 
- def compare_ratings(person1, person2):
+ def compare_ratings(self, person1, person2):
  # only check non-zero ratings
-  indices = np.where(self.myratings[person1] != 0) or np.where(self.myratings[person2] != 0)
+  indices = np.where(self.myratings[self.persons.index(person1)] != 0) or np.where(self.myratings[self.persons.index(person2)] != 0)
   if len(indices) == 0: return len(self.papers)*25+1 #5 is max difference;no basis to compare
-  cmp1 = self.myratings[person1][indices]
-  cmp2 = self.myratings[person2][indices]
-  return np.linalg.norm(self.myratings[cmp1] - self.myratings[cmp2])
+  cmp1 = self.myratings[self.persons.index(person1)][indices]
+  cmp2 = self.myratings[self.persons.index(person2)][indices]
+  return np.linalg.norm(cmp1 - cmp2)
 
  def recommendations(self):
   allrecommendations = {}
@@ -47,10 +47,6 @@ class ratings(object):
      closest = otherperson
      minimum = tmp
      notread = np.where(np.array(self.myratings[self.persons.index(otherperson)] == 0))[0]
-   print self.myratings
-   print self.persons.index(otherperson)
-   print notread
-   print len(notread)
    if len(notread) == 0:
     allrecommendations[person] = "No recommendation"
    else:
